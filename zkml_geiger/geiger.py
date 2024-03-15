9bf8839f60ad11637f0feed32d5acc43d4c2064c
lancenonce <lancendavis47@gmail.com>
2024-03-15 23:26:56 +0000

    feat: read geiger and import giza actions

--- zkml_geiger/geiger.py
+++ zkml_geiger/geiger.py
@@ -1,8 +1,14 @@
 from dotenv import load_dotenv
 from eth_account import Account
+from giza_actions import task, Action
 
 load_dotenv()
 
+@task
 def import_account(mnemonic):
     account = Account.from_mnemonic(mnemonic)
     return account
+
+# Read CSV files from the flipper zero. Forms the results into a numpy tensor
+def read_geiger():
+    print("Reading Geiger Counter data...")
