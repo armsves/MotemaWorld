76a9678cf346dfe8c32ded5471dfae934d3b6433
armsves <armsves@gmail.com>
2024-03-16 23:15:46 +0100

    added other components to integrate into main repo

---
+++ MotemaWRLDChromeExtension/model/content_script.js
@@ -0,0 +1,26 @@
+function listenPurchase() {
+    document.addEventListener("click", () => {
+        console.log("Main content script run");
+        productName = document.getElementById("productTitle")?.innerHTML;
+        currentpurchaseButton = document.getElementById('add-to-cart-button');
+        console.log("productName", productName);
+
+        if (currentpurchaseButton) {
+            currentpurchaseButton.addEventListener('click', function () {
+                chrome.runtime.sendMessage({ message: 'openNewTab', url: `https://near.org/armsveshack.near/widget/Motema?productName=${productName}` });
+            });
+            chrome.runtime.sendMessage({message: 'changeIcon', color: 'green'});
+        }
+
+    })
+
+}
+
+function sendRefreshRequiredRequest() {
+    // noinspection JSUnresolvedFunction,JSUnresolvedVariable
+    chrome.runtime.sendMessage({ msg: REFRESH_REQUIRED_REQUEST });
+    console.log("MSG SENT");
+
+}
+
+listenPurchase();
