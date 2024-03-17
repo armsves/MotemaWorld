4908cb338354a729195d092e2cc9cb880b0d8d38
armsves <armsves@gmail.com>
2024-03-17 07:39:36 +0100

    minor ui changes to make everything look the same

--- src/pages/verify-miner.tsx
+++ src/pages/verify-miner.tsx
@@ -92,12 +92,14 @@ export default function Home() {
 
     return (
         <>
-            <div>
-                <Link href="/" style={{ display: 'inline-block', margin: '10px', padding: '10px', backgroundColor: '#0070f3', color: '#fff', borderRadius: '5px', textDecoration: 'none' }}>Home</Link>
-                <Link href="/create-miner" style={{ display: 'inline-block', margin: '10px', padding: '10px', backgroundColor: '#0070f3', color: '#fff', borderRadius: '5px', textDecoration: 'none' }}>Create Miner</Link>
-                <Link href="/verify-miner" style={{ display: 'inline-block', margin: '10px', padding: '10px', backgroundColor: '#0070f3', color: '#fff', borderRadius: '5px', textDecoration: 'none' }}>Verify Miner</Link>
+            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                <img src="https://i.ibb.co/1T3Jd3m/banner.jpg" alt="Motema Banner" />
+                <div>
+                    <Link href="/" style={{ display: 'inline-block', margin: '10px', padding: '10px', backgroundColor: '#0070f3', color: '#fff', borderRadius: '5px', textDecoration: 'none' }}>Home</Link>
+                    <Link href="/create-miner" style={{ display: 'inline-block', margin: '10px', padding: '10px', backgroundColor: '#0070f3', color: '#fff', borderRadius: '5px', textDecoration: 'none' }}>Create Miner</Link>
+                    <Link href="/verify-miner" style={{ display: 'inline-block', margin: '10px', padding: '10px', backgroundColor: '#0070f3', color: '#fff', borderRadius: '5px', textDecoration: 'none' }}>Verify Miner</Link>
+                </div>
             </div>
-
             <div>
                 <div className="flex flex-col items-center justify-center align-middle">
                     <p className="text-2xl mb-5">Miner verification page</p>
